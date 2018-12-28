14a9ac9c31d152c7b73094050a0efe70a37eb971
Jorge Gonzalez Soria <jorggo06@ucm.es>
2018-12-28 15:03:49 +0100

    Add files via upload

---
+++ examen/examen/FINAL/ejercicio3/main.cpp
@@ -0,0 +1,106 @@
+/**
+  NOMBRE Y APELLIDOS:
+  LABORATORIO:
+  PUESTO:
+  USUARIO DE DOMJUDGE USADO EN EL EXAMEN:
+*/
+
+
+#include "Arbin.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+/**
+  COMPLEJIDAD: Explica aquí justificadamente cuál es la complejidad de 
+               esta función
+			   ***A COMPLETAR***
+*/
+unsigned int numero_singulares(Arbin<int> a) {
+	// A IMPLEMENTAR
+}
+
+Arbin<int> lee_arbol(istream& in) {
+	char c;
+	in >> c;
+	switch (c) {
+	case '#': return Arbin<int>();
+	case '[': {
+		int raiz;
+		in >> raiz;
+  		in >> c;
+		return Arbin<int>(raiz);
+	}
+	case '(': {
+		Arbin<int> iz = lee_arbol(in);
+		int raiz;
+		in >> raiz;
+		Arbin<int> dr = lee_arbol(in);
+		in >> c;
+		return Arbin<int>(iz, raiz, dr);
+	}
+	default: return Arbin<int>();
+	}
+}
+
+/**
+Este programa lee árboles de la entrada estándar, 
+codificados de acuerdo con las siguientes reglas:
+(1) El árbol vacío se codifica como #
+(2) Los árboles simples se codifican como [v], con  
+    v el valor del nodo
+(3) Los árboles complejos se codifican como (IvD),
+    con I la codificación del hijo izquierdo,
+	v el valor de la raíz, y D la codificación
+	del hijo derecho.
+Para cada árbol leído, escribe en la salida estádar
+el número de nodos singulares de dicho árbol.
+	
+Algunos casos representativos:
+
+#
+[0]
+[5]
+(([4]3[-3])1[-4])
+
+-El primero representa el árbol vacío. Al no tener nodos,
+un árbol vacío tiene 0 nodos sigulares.
+-El segundo representa un árbol con un único nodo, cuyo valor
+es 0. Dicho nodo es singular, ya que: (i) como no tiene 
+ascestros, la suma de los ancestros es 0, (ii) como no
+tiene descendientes, la suma de los descendientes es 0. 
+-El tercero representa otro árbol con un único nodo, cuyo
+valor es 5. Por la misma razón que en el segundo ejemplo,
+este nodo es singular.
+-El cuarto representa el siguiente árbol:
+     
+	       1
+		  /  \
+		  3  -4
+         / \  
+        4  -3 
+
+Este árbol tiene 2 nodos sigulares: la raíz, y la raíz del hijo 
+izquierdo. 
+
+Por tanto, la salida del programa para estos casos será:
+
+0
+1
+1
+2
+
+*/
+
+int main() {
+	Arbin<int> arbol;
+	while (cin.peek() != EOF)  {
+		cout << numero_singulares(lee_arbol(cin));
+		string resto_linea;
+		getline(cin, resto_linea);
+		if (cin.peek() != EOF) cout << endl;
+	}
+	return 0;
+}
---
+++ examen/examen/PARCIAL/ejercicio3/GestionAdmisiones.cpp
@@ -0,0 +1,80 @@
+/**
+  NOMBRE Y APELLIDOS:
+  LABORATORIO:
+  PUESTO:
+  USUARIO DE DOMJUDGE USADO EN EL EXAMEN:
+*/
+
+#include "GestionAdmisiones.h"
+
+/** 
+Implementación de las operaciones de la clase Paciente 
+*/
+
+Paciente::Paciente(const string& nombre, unsigned int edad, const string& sintomas):
+    _nombre(nombre),  _sintomas(sintomas), _edad(edad) {}	
+
+unsigned int Paciente::edad() const {
+   return _edad;	
+}
+const string& Paciente::nombre() const {
+   return _nombre;	
+}
+const string& Paciente::sintomas() const {
+   return _sintomas;	
+}
+
+/**
+Implementa aquí los métodos de las clases adicionales
+*/
+
+ 
+/**
+Debes completar la implementación de las operaciones de GestionAdmisiones,
+y justificar la complejidad de los mismos.
+*/ 
+
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+GestionAdmisiones::GestionAdmisiones() {
+	// A IMPLEMENTAR
+	
+}
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+void GestionAdmisiones::an_paciente(CodigoPaciente codigo, const string& nombre, unsigned int edad, const string& sintomas, Gravedad gravedad) {
+	// A IMPLEMENTAR
+}
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+void GestionAdmisiones::info_paciente(CodigoPaciente codigo, string& nombre, unsigned int& edad, string& sintomas) const {
+	// A IMPLEMENTAR
+}
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+void GestionAdmisiones::siguiente(CodigoPaciente& codigo, Gravedad& gravedad) const {
+	// A IMPLEMENTAR
+}
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+bool GestionAdmisiones::hay_pacientes() const {  
+	// A IMPLEMENTAR
+}
+
+/**
+ COMPLEJIDAD: Determina y justifica aquí la complejidad de la operación
+*/
+void GestionAdmisiones::elimina(CodigoPaciente codigo) {
+	// A IMPLEMENTAR
+}
+   
